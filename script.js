f82de7a12adc92cd73e73113e5aef1a5334c7f15
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-05-01 10:30:59 +0700

    Add Marker data definition

--- script.js
+++ script.js
@@ -3,6 +3,32 @@
 
 
 // Constant Declarations
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 const startButton = document.querySelector("#startButton");
 const endButton = document.querySelector("#endButton");
+
+
+// Data Definition
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Marker is one of :
+// - "X"
+// - "O"
+// interp. the mark put by player into board
+
+/*
+function  fnForMarker(marker) {
+    switch(marker) {
+        case "X":
+            return ...;
+        case "O":
+            return ...;
+    }
+}
+*/
+
+// Template rule used:
+//  - One of: 2 cases
+//  - Atomic distinct: "X"
+//  - Atomic distinct: "O"
 
