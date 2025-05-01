c0826fae649a61491c8d5fe3c310a82145d7972d
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-05-01 09:59:37 +0700

    Add constant declaration for buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// TIC TAC TOE Game
+
+
+// Constant Declarations
+const startButton = document.querySelector("#startButton");
+const endButton = document.querySelector("#endButton");
+
+console.log(startButton);
+console.log(endButton); 
