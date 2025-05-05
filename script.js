6aa7473bb7093c20333eb45b353e4c2ff985951f
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-05-05 10:31:46 +0700

    Add createPlayer function using factory pattern

--- script.js
+++ script.js
@@ -140,3 +140,18 @@ function funForIndex(index) {
 */
 // Template rule used:
 //  - Atomic non distinct: Natural number
+
+
+// Player is createPlayer(marker, name, playerState, lightState)
+function createPlayer(marker,name, playerState, lightState, idName, idPlayerState, idLightState, idWin) {
+    let state = playerState;
+    let light = lightState;
+    let win = 0;
+    const nameEl = document.querySelector("#" + idName);
+    const playerStateEl = document.querySelector("#" + idPlayerState);
+    const lightStateEl = document.querySelector("#"+ idLightState);
+    const winEl = document.querySelector("#" + idWin) ; 
+    const setState = (newState) => state = newState;
+    const setLight = (newLight) => light = newLight;
+    return {marker, name, state, light, win, nameEl, playerStateEl, lightStateEl, winEl, setState, setLight}
+}
